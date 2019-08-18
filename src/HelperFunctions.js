1c5ba94587d9ec3ed6eaffb5f87b1139b91082f4
Peter Nguyen <nguyenvandung02051999@gmail.com>
2019-08-18 12:05:44 -0700

    added accurateInterval

--- src/HelperFunctions.js
+++ src/HelperFunctions.js
@@ -24,3 +24,25 @@ export const secondToTimeString = (secondAsWhole = 0) => {
     return `${minute.toString()}:${second.toString()}`;
 };
 
+export function accurateInterval(fn, time) {
+    (function() {
+        window.accurateInterval = function(fn, time) {
+            var cancel, nextAt, timeout, wrapper;
+            nextAt = new Date().getTime() + time;
+            timeout = null;
+            wrapper = function() {
+                nextAt += time;
+                timeout = setTimeout(wrapper, nextAt - new Date().getTime());
+                return fn();
+            };
+            cancel = function() {
+                return clearTimeout(timeout);
+            };
+            timeout = setTimeout(wrapper, nextAt - new Date().getTime());
+            return {
+                cancel: cancel
+            };
+        };
+    }).call(this);
+  }
+
