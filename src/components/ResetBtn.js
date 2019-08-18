7cd4b83bda03c2c1c798bc2c4d44fc0976e2d00e
Peter Nguyen <nguyenvandung02051999@gmail.com>
2019-08-18 15:17:07 -0700

    Styling, moving right along

--- src/components/ResetBtn.js
+++ src/components/ResetBtn.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export function ResetBtn(props) {
   return (
-    <div id="reset" onClick={props.resetAll} className='node col-md-1'>
-      <div  className='icono-sync'/>
+    <div id="reset" onClick={props.resetAll} className='node'>
+      <div className='icono-sync'/>
     </div>
   );
 }
